765fd72a8519d7a971265ec08b1e307e4fdbb6f9
Saysamiche <140890694+Saysamiche@users.noreply.github.com>
2025-05-07 17:19:39 +0100

    cleaning up codes

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-let menu;
+const openMenu = document.getElementById("openMenu");
+const closeMenu = document.getElementById("closeMenu");
+const navBar = document.getElementById("navigation");
+
+openMenu.addEventListener("click", () => {
+  navBar.classList.add("menu");
+  openMenu.classList.add("open");
+  closeMenu.classList.add("close");
+});
+
+closeMenu.addEventListener("click", () => {
+  navBar.classList.remove("menu");
+  openMenu.classList.remove("open");
+  closeMenu.classList.remove("close");
+});
